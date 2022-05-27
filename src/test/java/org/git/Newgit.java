8f25dcd2ef46f9bb5cc34b29e746cc69972b897e
vinoth418 <vinovinoth418887@gmail.com>
2022-05-27 21:43:00 +0530

    methodsadded

---
+++ src/test/java/org/git/Newgit.java
@@ -0,0 +1,24 @@
+package org.git;
+
+public class Newgit {
+
+	
+	private void method1() {
+		System.out.println("Vinoth");
+
+	}
+	private void method2() {
+		System.out.println("rajendran");
+	}
+private void method3() {
+	System.out.println("sasi");
+
+}
+private void method4() {
+	System.out.println("yogesh");
+
+}
+
+	
+	
+}
